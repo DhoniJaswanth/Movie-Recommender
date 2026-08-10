fbe94519a2bf25777a4623d9a83c4bf4a536aafe
DhoniJaswanth <dhonijaswanth22@gmail.com>
2026-08-10 11:09:22 +0530

    app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def poster_grid(cards, cols=6, key_prefix="grid"):
 
             with colset[c]:
                 if poster:
-                    st.image(poster, use_container_width="stretch")
+                    st.image(poster, use_container_width=True)
                 else:
                     st.write("🖼️ No poster")
 
